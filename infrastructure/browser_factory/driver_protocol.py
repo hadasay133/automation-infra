b7ce81c611baef1da734581fbd884a08deb80bcc
unknown <hadasay133@gmail.com>
2025-09-08 16:55:03 +0300

    Clean main branch to allow full code PR

--- infrastructure/browser_factory/driver_protocol.py
+++
@@ -1,7 +0,0 @@
-from typing import Protocol, Optional, List
-
-from selenium.webdriver.remote.webdriver import WebDriver
-
-
-class DriverProtocol(Protocol):
-    def get_driver(self,browser_name: str, is_remote: bool, options: Optional[List[str]]) -> WebDriver:...
